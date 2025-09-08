b8d5a671875f623bab1b94c4d3b6dbfaead8e750
nameerqadir <nameer.qadir@mail.utoronto.ca>
2025-09-08 13:22:42 -0400

    up to task 4.1

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int multiple = multiples(1000,3,5);
+        System.out.println(multiple);
+    }
+
+     static int multiples(int n, int a, int b) {
+        int counter = 0;
+        for (int i = 1; i < n ;i++) {
+           boolean divisibleBy3 = i % a == 0;
+           boolean divisibleBy5 = i % b == 0;
+
+           if (divisibleBy3 || divisibleBy5) {
+               counter++;
+           }
+
+
+        }
+        return counter;
+    }
+}
