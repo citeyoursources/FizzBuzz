fffc560a46b9c6dc8b998ae3c4478da7176787f7
nameerqadir <nameer.qadir@mail.utoronto.ca>
2025-09-08 13:31:32 -0400

    up to overloading

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,7 +3,9 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
         int multiple = multiples(1000,4,5);
+        int multiple1 = multiples();
         System.out.println(multiple);
+        System.out.println(multiple1);
     }
 
      public static int multiples(int n, int a, int b) {
